8aa43603833e91fb17c016d542a36a24997297d2
Andybocil <158800993+Andybocil@users.noreply.github.com>
2024-08-04 20:43:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,15 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-import asyncio
-from pyrogram import idle
+from bot import Bot, data, Database
 
-from bot import Bot, Database, data
-
-
-async def reset_menfess(bot):
+async def reset_menfess():
     db = Database(data[0])
-    try:
-        x = await db.reset_menfess()
-        await bot.kirim_pesan(x=str(x))
-        print("BOT BERHASIL DIRESET")
-    except Exception as e:
-        print(f"Error during reset: {str(e)}")
+    x = await db.reset_menfess()
+    await Bot().kirim_pesan(x=str(x))
+    print('BOT BERHASIL DIRESET')
 
+scheduler = AsyncIOScheduler(timezone="Asia/Jakarta")
+scheduler.add_job(reset_menfess, trigger="cron", hour=1, minute=0)
+scheduler.start()
 
-async def main():
-    bot = Bot()
-    
-    scheduler = AsyncIOScheduler(timezone="Asia/Jakarta")
-    scheduler.add_job(reset_menfess, args=[bot], trigger="cron", hour=1, minute=0)
-    scheduler.start()
-    
-    await bot.start()
-    print(f"{bot.username} BOT TELAH AKTIF")
-    
-    try:
-        await idle()  # Pyrogram idle to keep the bot running
-    finally:
-        scheduler.shutdown()
-        await bot.stop()
-        print("BOT BERHASIL DIHENTIKAN")
+Bot().run()
 
-if __name__ == "__main__":
-    asyncio.run(main())
