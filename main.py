1c8614106f72f42481b481669bd904006795048f
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-08-05 15:36:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
+import asyncio
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from bot import Bot, data, Database
 
 async def reset_menfess():
     db = Database(data[0])
     x = await db.reset_menfess()
-    await Bot().kirim_pesan(x=str(x))
+    bot = Bot()  
+    await bot.kirim_pesan(x=str(x))
     print('BOT BERHASIL DIRESET')
 
-scheduler = AsyncIOScheduler(timezone="Asia/Jakarta")
-scheduler.add_job(reset_menfess, trigger="cron", hour=1, minute=0)
-scheduler.start()
+async def main():
+    bot = Bot()
+    scheduler = AsyncIOScheduler(timezone="Asia/Jakarta")
+    scheduler.add_job(reset_menfess, trigger="cron", hour=1, minute=0)
+    scheduler.start()
+    
+    await bot.start()
+    
+    try:
+        await asyncio.Event().wait()
+    except (KeyboardInterrupt, SystemExit):
+        await bot.stop()
 
-Bot().run()
-
+if __name__ == "__main__":
+    asyncio.run(main())
