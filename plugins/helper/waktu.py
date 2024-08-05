1a493ac07c54f4523b9cab3326a847bc424941ae
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-08-05 15:41:35 +0700

    Update waktu.py

--- plugins/helper/waktu.py
+++ plugins/helper/waktu.py
@@ -14,5 +14,16 @@ class Waktu:
         return (f"Hari: {self.hari}, Tanggal: {self.tanggal}, Bulan: {self.bulan}, "
                 f"Tahun: {self.tahun}, Jam: {self.jam}, Full Time: {self.full_time}")
 
+# Definisikan data
+data = {
+    'hari': 'Senin',
+    'tanggal': '04',
+    'bulan': 'Agustus',
+    'tahun': '2024',
+    'jam': '10:00',
+    'full': 'Senin, 04 Agustus 2024, 10:00 WIB'
+}
+
+# Membuat instance Waktu
 waktu = Waktu(data)
 print(waktu)
